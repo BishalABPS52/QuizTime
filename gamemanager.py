326d76df762c4b9508cead493e1e9e78d07db598
Bishal Shrestha <167290532+BishalABPS52@users.noreply.github.com>
2025-07-17 20:45:07 +0545

    Update gamemanager.py

--- gamemanager.py
+++ gamemanager.py
@@ -19,7 +19,14 @@ GOLDEN = (255, 215, 0)
 
 # Button Class
 class Button:
-    
+    def __init__(self, text, font, color, rect, bg_color=GOLDEN):
+        self.text = text
+        self.font = font
+        self.color = color
+        self.rect = pygame.Rect(rect)
+        self.bg_color = bg_color
+        self.text_surf = self.font.render(self.text, True, self.color)
+        self.text_rect = self.text_surf.get_rect(center=self.rect.center)
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.bg_color, self.rect)
